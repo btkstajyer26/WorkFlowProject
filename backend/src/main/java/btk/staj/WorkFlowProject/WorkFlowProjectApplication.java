ee3f14991ed42de4b917fd53d818fc7efbeb9d64
btkstajyer26 <btkstajyer26@gmail.com>
2026-08-04 10:18:48 +0300

    init

---
+++ backend/src/main/java/btk/staj/WorkFlowProject/WorkFlowProjectApplication.java
@@ -0,0 +1,13 @@
+package btk.staj.WorkFlowProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkFlowProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/btk/staj/WorkFlowProject/WorkFlowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package btk.staj.WorkFlowProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
