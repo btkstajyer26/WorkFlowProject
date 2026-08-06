92dc5df5130c81ac3c11a91fe5c901a14e6d69ef
Nisan Tat <nisantat10@gmail.com>
2026-08-06 16:35:10 +0300

    kimlik doğrulama

---
+++ backend/src/main/java/btk/staj/WorkFlowProject/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package btk.staj.WorkFlowProject.user.repository;
+
+import btk.staj.WorkFlowProject.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
