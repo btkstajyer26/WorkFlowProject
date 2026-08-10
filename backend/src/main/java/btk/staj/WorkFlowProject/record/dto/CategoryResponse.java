c932077fd07b7f5200c48fe167f9ccdf7a876be7
Fevzi Berke URGANİOĞLU <fberkeurgani@gmail.com>
2026-08-10 15:25:27 +0300

    category files eklendi.

---
+++ backend/src/main/java/btk/staj/WorkFlowProject/record/dto/CategoryResponse.java
@@ -0,0 +1,9 @@
+package btk.staj.WorkFlowProject.record.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponse {
+    private Integer id;
+    private String name;
+}
---
+++ backend/src/main/java/btk/staj/WorkFlowProject/record/entity/Category.java
@@ -0,0 +1,17 @@
+package btk.staj.WorkFlowProject.record.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+}
