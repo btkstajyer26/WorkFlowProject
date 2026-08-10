9207c147608097b84756747408580f784af98da9
Fevzi Berke URGANİOĞLU <fberkeurgani@gmail.com>
2026-08-10 13:56:52 +0300

    repository içerisindeki statuslar workflowdan import edildi. kayıt dosyaları için ileride düzenlenmek üzere recordfile dosyası açıldı.

---
+++ backend/src/main/java/btk/staj/WorkFlowProject/record/entity/RecordFile.java
@@ -0,0 +1,5 @@
+package btk.staj.WorkFlowProject.record.entity;
+
+public class RecordFile {
+//Bu sınıf dosyanın GUID adını, orijinal adını, uzantısını ve boyutunu tutacak. İleride eklenecek. :)    
+}
